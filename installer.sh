3dab19fa6cce464461f73448775e57d4a7b84ebc
Shubham Tandon <iamshubham316@gmail.com>
2024-07-16 17:00:08 +0000

    temp(installer): test script for checking present package

--- installer.sh
+++ installer.sh
@@ -1,3 +1,23 @@
-!#/usr/bin/bash
+#!/usr/bin/bash
 
 echo "Hello World!"
+
+# exec neofetch
+
+# echo "zsh or bash? [zsh/bash] "
+#
+# read script
+#
+# if [[ script=="zsh" ]]; then
+# 	exec which zsh
+# else
+# 	exec which bash
+# fi
+
+PACKAGE="neofetch"
+
+if apt list --installed 2>/dev/null | grep -qw "$PACKAGE"; then
+	echo "$PACKAGE is installed."
+else
+	echo "$PACKAGE is not installed."
+fi
