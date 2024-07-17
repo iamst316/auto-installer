cc7505814a16c4e0f153c6705290eb305cd080f3
Abhay Tiwari <abhykrs@gmail.com>
2024-07-17 12:00:54 +0000

    add(installer): added logic for shell installation

--- installer.sh
+++ installer.sh
@@ -2,24 +2,37 @@
 
 echo "Hello World!"
 
-# exec neofetch
-
-# echo "zsh or bash? [zsh/bash] "
-#
-# read script
-#
-# if [[ script=="zsh" ]]; then
-# 	exec which zsh
-# else
-# 	exec which bash
-# fi
-
 PACKAGE="neofetch"
 
 if apt list --installed | grep -qw "$PACKAGE"; then
-	echo "$PACKAGE is installed."
+  echo "$PACKAGE is installed."
 else
-	# echo "$PACKAGE is not installed."
-	exec sudo apt install -y $PACKAGE
-	# echo "$PACKAGE installed"
+  # echo "$PACKAGE is not installed."
+  exec sudo apt install -y $PACKAGE
+  # echo "$PACKAGE installed"
 fi
+
+# a custom shell will be installed now
+
+# echo "Do You Want to Install a custom shell? [y/n]"
+
+validity = false
+
+while [ "$validity" = false ]; do
+  echo "Enter your choice (y/n): "
+  read choice
+
+  case "$choice" in
+  y | Y)
+    validity=true
+    echo "You entered 'yes'."
+    ;;
+  n | N)
+    validity=true
+    echo "You entered 'no'."
+    ;;
+  *)
+    echo "Invalid input. Please enter 'y/Y' or 'n/N'."
+    ;;
+  esac
+done
