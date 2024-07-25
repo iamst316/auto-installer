c691c01e8be7769ab94a0e9de7eddc3363bf2dee
Shubham Tandon <iamshubham316@gmail.com>
2024-07-25 06:08:52 +0000

    merge(branch): mered devbranch

--- installer.sh
+++ installer.sh
@@ -5,11 +5,11 @@ echo "Hello World!"
 PACKAGE="neofetch"
 
 if apt list --installed | grep -qw "$PACKAGE"; then
-  echo "$PACKAGE is installed."
+	echo "$PACKAGE is installed."
 else
-  # echo "$PACKAGE is not installed."
-  exec sudo apt install -y $PACKAGE
-  # echo "$PACKAGE installed"
+	# echo "$PACKAGE is not installed."
+	exec sudo apt install -y $PACKAGE
+	# echo "$PACKAGE installed"
 fi
 
 # a custom shell will be installed now
@@ -19,20 +19,20 @@ fi
 validity = false
 
 while [ "$validity" = false ]; do
-  echo "Enter your choice (y/n): "
-  read choice
+	echo "Enter your choice (y/n): "
+	read choice
 
-  case "$choice" in
-  y | Y)
-    validity=true
-    echo "You entered 'yes'."
-    ;;
-  n | N)
-    validity=true
-    echo "You entered 'no'."
-    ;;
-  *)
-    echo "Invalid input. Please enter 'y/Y' or 'n/N'."
-    ;;
-  esac
+	case "$choice" in
+	y | Y)
+		validity=true
+		echo "You entered 'yes'."
+		;;
+	n | N)
+		validity=true
+		echo "You entered 'no'."
+		;;
+	*)
+		echo "Invalid input. Please enter 'y/Y' or 'n/N'."
+		;;
+	esac
 done
